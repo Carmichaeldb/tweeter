d42b31c3cd48e69d99e696b6ba31ee53b775fa1e
David Carmichael <carmichael.db88@gmail.com>
2023-11-12 03:18:28 +0000

    Add: composer-char-counter script and implement logic

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+/**
+ * Count user input in tweet-text textarea.
+ * Displays number of characters in textarea.
+ * Changes colour if number is above character limit.
+ */
+
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    const textAreaInput = $(this).val().length;
+    const charCount = 140 - textAreaInput;
+    if (charCount < 0) {
+      $(".counter").text(charCount).css({"color": "red"});
+    }
+    $(".counter").text(charCount);
+  });
+});
