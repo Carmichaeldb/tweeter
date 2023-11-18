a1f0bee54fb25b80a2814a7094f0902a30022ea3
David Carmichael <carmichael.db88@gmail.com>
2023-11-18 00:06:52 +0000

    Add: Ajax and Jquery logic to load tweets, Edit: index page to display tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,51 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $("#tweets-container").append(createTweetElement(tweet));
+    }
+  };
+
+  const createTweetElement = function(tweet) {
+    let postDate = new Date(tweet.created_at);
+    let $tweet = $(`
+    <article>
+    <header>
+    <img src="${tweet.user.avatars}" alt="Users Profile Image">
+    <p>${tweet.user.name}</p>
+    <a class="tweet-user" href="">${tweet.user.handle}</a>
+    </header>
+    <main>
+    <p>${tweet.content.text}</p>
+    </main>
+    <footer>
+    <p>${postDate.toDateString()}</p>
+    <div class="tweet-icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-arrows-rotate"></i><i class="fa-solid fa-heart"></i></div>
+    </footer>
+    </article>`);
+    return $tweet;
+  };
+
+  const loadTweets = function() {
+    $.ajax("/tweets", {
+      method: "GET",
+      datType: "JSON",
+      success: (data) => { renderTweets(data); }
+    }
+    );
+  };
+
+  loadTweets();
+
+  $(".tweet-form").on("submit", function(event) {
+    event.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/tweets/",
+      data: $("form").serialize()
+    });
+  });
+});
